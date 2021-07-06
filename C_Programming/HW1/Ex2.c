d3f5815ae7d97222d4f7739a7eceb01139df95d5
Abdallah7Salem <abdallahalaasalem7@gmail.com>
2021-07-06 20:54:12 +0200

    Adding my 2nd assignment

---
+++ C_Programming/HW1/Ex2.c
@@ -0,0 +1,18 @@
+/*
+ * Ex2.c
+ *
+ *  Created on: Jul 4, 2021
+ *      Author: Abdallah Salem
+ */
+
+// C program to print an integer entered by a user
+#include "stdio.h"
+#include "stdlib.h"
+
+void main()
+{
+	int number;
+	printf("Enter an integer: ");
+	scanf("%d", &number);
+	printf("You entered: %d", number);
+}
