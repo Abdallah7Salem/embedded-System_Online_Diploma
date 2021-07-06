33186d62682b5a7217b9deec3bd889f39581d0f4
Abdallah7Salem <abdallahalaasalem7@gmail.com>
2021-07-06 20:58:42 +0200

    Adding my 1st question is assignment

---
+++ C_Programming/HW1/Ex1.c
@@ -0,0 +1,14 @@
+/*
+ * Ex2.c
+ *
+ *  Created on: Jul 4, 2021
+ *      Author: Abdallah Salem
+ */
+
+// C program to print " C Programming "
+# include "stdio.h"
+
+void main()
+{
+	printf("C Programming");
+}
