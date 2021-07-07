4722743c39872ca97e3e15cd598a613925f25211
Abdallah7Salem <abdallahalaasalem7@gmail.com>
2021-07-07 04:53:29 +0200

    Adding solution of Ex6

---
+++ C_Programming/HW1/Ex6.c
@@ -0,0 +1,19 @@
+// Source code to swap two numbers  
+
+#include "stdio.h"
+
+void main()
+{
+	float a, b, temp;
+	printf("Enter value of a: ");
+	scanf("%f", &a);
+	printf("Enter value of b: ");
+	scanf("%f", &b);
+	
+	temp = a;
+	a = b;
+	b = temp;
+	
+	printf("\nAfter swapping, value of a = %0.2f", a);
+	printf("\nAfter swapping, value of b = %0.2f", b);
+}
