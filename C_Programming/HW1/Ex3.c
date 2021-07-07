decf9f4574218c4c66f0c894ef3af8b588c7abfa
Abdallah7Salem <abdallahalaasalem7@gmail.com>
2021-07-07 04:34:18 +0200

    Adding the solution of Ex3 in Assignment 1

---
+++ C_Programming/HW1/Ex3.c
@@ -0,0 +1,15 @@
+// C Program to add 2 integers
+
+#include "stdio.h"
+
+void main()
+{
+	int x, y;
+	printf("Enter two integers: ");
+	scanf("%d\n%d", &x, &y);
+	printf("Sum: %d", x + y);
+}
+
+
+
+
