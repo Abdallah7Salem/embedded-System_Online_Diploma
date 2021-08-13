8ee6273624a3ae4bcc40cb61b19896c21e14da0a
Abdallah <abdallahalaasalem7@gmail.com>
2021-08-13 08:12:49 +0200

    Homework6 - Pointers - Ex4

---
+++ C_Programming/HW6/Ex4.c
@@ -0,0 +1,35 @@
+// C Program to print the elements of an array in reverse order
+
+#include "stdio.h"
+
+
+int main(void)
+{
+
+	int size;
+
+	printf("input the number of the elements to store in the array(max 15) : ");
+	scanf("%d", &size);
+
+	int arr[size];
+
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		printf("\nelement - %d : ", i+1);
+		scanf("%d", &arr[i]);
+	}
+
+	int* p_arr;
+	p_arr = &arr[size - 1];
+
+	for(i = 0; i < size; i++)
+	{
+		printf("\nelement - %d : %d", size-i-1, *p_arr);
+		p_arr--;
+	}
+
+
+	return 0;
+}
+
