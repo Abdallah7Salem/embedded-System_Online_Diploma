b9f55eb742e35b175defae4805fd18b50db38c32
Abdallah <abdallahalaasalem7@gmail.com>
2021-11-03 14:03:17 +0200

    Unit 7 - Section 1 - Task 1

---
+++ Unit7_MCU_Essential_Peripherals/Section1/Memorymap.h
@@ -0,0 +1,18 @@
+/*
+ * Memorymap.h
+ *
+ * Created: 11/3/2021 12:36:35 PM
+ *  Author: Abdallah
+ */ 
+
+
+#ifndef MEMORYMAP_H_
+#define MEMORYMAP_H_
+
+#define PORTA *((volatile unsigned char *)0x3B)
+#define DDRA  *((volatile unsigned char *)0x3A)
+#define PINA  *((volatile unsigned char *)0x39)
+
+
+
+#endif /* MEMORYMAP_H_ */
